b4a4b6e073f9e6e18bcffb9708fa1e90f8f901de
Wonder Gonzalez <gonzalezwonderly@gmail.com>
2022-02-21 02:44:48 -0400

    [chore] Form Yup basic set up

--- src/Form.js
+++ src/Form.js
@@ -5,39 +5,61 @@ import {
   FormLabel,
   FormControl,
   FormHelperText,
+  Button,
 } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import React from "react";
+
+const formSchema = yup
+  .object({
+    name: yup.string().required("Este campo es requerido"),
+  })
+  .required();
 
 const UserForm = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm({
+    resolver: yupResolver(formSchema),
+  });
   const onSubmit = (data) => console.log(data);
   return (
     <VStack
       as="form"
       minWidth="30%"
       bgColor="#FFF"
-      padding="3em"
+      padding="2em"
       borderRadius="12px"
       shadow="md"
       mt="4em"
-      onSubmit={handleSubmit(onSubmit)}
+      onSubmit={handleSubmit(onSubmit, {})}
     >
       <Heading>User details</Heading>
       <FormControl>
         <FormLabel htmlFor="name">Name</FormLabel>
-        <Input id="name" type="text" {...register("name")} />
+        <Input type="text" {...register("name")} />
         <FormHelperText>Your first name</FormHelperText>
       </FormControl>
-      <FormControl>
-        <FormLabel htmlFor="email">Email address</FormLabel>
+      {/* <FormControl>
+        <FormLabel htmlFor="email" {...register("email")}>
+          Email address
+        </FormLabel>
         <Input id="email" type="email" />
         <FormHelperText>We'll never share your email.</FormHelperText>
       </FormControl>
       <FormControl>
         <FormLabel htmlFor="email">Phone Number</FormLabel>
-        <Input id="email" type="email" />
+        <Input id="email" {...register("phoneNumber")} />
         <FormHelperText>Your daily phone number is fine.</FormHelperText>
-      </FormControl>
+      </FormControl> */}
+      <Button
+        type="submit"
+        colorScheme="blue"
+        backgroundColor="cyan.400"
+        width="100%"
+      >
+        Submit
+      </Button>
     </VStack>
   );
 };
